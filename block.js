8a654e02e090cc5975517cbd8c4649191b57e2cd
Jemimakapoor <75304956+Jemimakapoor@users.noreply.github.com>
2021-05-06 16:37:42 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,31 @@
+class Block{
+    constructor(x, y, width, height, angle) {
+      this.visibility=255
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        if(this.body.speed<3){
+
+        
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect( 0, 0, this.width, this.height);
+        pop();
+      }
+      else{
+        World.remove(world,this.body)
+      }
+      }
+}
